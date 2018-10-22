bc672969554185286b1a3bc7c3e0fcbe602ad565
Isaque Ribeiro <isaque.ribeiro.bernardes@everis.com>
2018-10-22 16:55:03 -0300

    Complete API
    First commit containing the entire project, complete api

---
+++ DevStore.Api/Controllers/ProductController.cs
@@ -0,0 +1,128 @@
+﻿using DevStore.DataAccess.DataContexts;
+using DevStore.Domain;
+using System;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DevStore.Api.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private DevStoreDataContext db = new DevStoreDataContext();
+
+        [Route("products")]
+        public HttpResponseMessage GetProducts()
+        {
+            var result = db.Products.Include("Category").ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        [Route("categories")]
+        public HttpResponseMessage GetGategories()
+        {
+            var result = db.Categories.ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        [Route("categories/{categoryId}/products")]
+        public HttpResponseMessage GetProductsByCategories(int categoryId)
+        {
+            var result = db.Products.Include("Category").Where(x => x.CategoryId == categoryId).ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        [HttpPost]
+        [Route("products")]
+        public HttpResponseMessage PostProduct(Product product)
+        {
+            if(product == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                var result = product;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Falha ao incluir produto");
+            }
+
+        }
+
+        [HttpDelete]
+        [Route("products")]
+        public HttpResponseMessage DeleteProduct(int productId)
+        {
+            if(productId <= 0)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            try
+            {
+                Product productToRemove = new Product();
+                productToRemove = db.Products.Find(productId);
+
+                db.Products.Remove(productToRemove);
+                db.SaveChanges();
+
+                return Request.CreateResponse($"Produto '{productToRemove.Title}' excluido.");
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpPut]
+        [Route("products")]
+        public HttpResponseMessage PutProducts(Product product)
+        {
+            if (product == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            try
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+
+                var result = product;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpPatch]
+        [Route("products")]
+        public HttpResponseMessage PatchProduct(Product product)
+        {
+            if (product == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            try
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+
+                var result = product;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+                db.Dispose();
+        }
+    }
+}
---
+++ DevStore.DataAccess/DataContexts/DevStoreDataContext.cs
@@ -0,0 +1,50 @@
+﻿using DevStore.Domain;
+using DevStore.Infra.Mappings;
+using System.Data.Entity;
+
+namespace DevStore.DataAccess.DataContexts
+{
+    public class DevStoreDataContext : DbContext
+    {
+        public DevStoreDataContext() : 
+            base("DevStoreConnectionString")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+            Database.SetInitializer<DevStoreDataContext>(new DevStoreDataContextInitializer());
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public class DevStoreDataContextInitializer : DropCreateDatabaseIfModelChanges<DevStoreDataContext>
+        {
+            protected override void Seed(DevStoreDataContext context)
+            {
+                context.Categories.Add(new Category { Id = 1, Title = "Informática" });
+                context.Categories.Add(new Category { Id = 2, Title = "Games" });
+                context.Categories.Add(new Category { Id = 3, Title = "Papelaria" });
+                context.SaveChanges();
+
+                context.Products.Add(new Product { Id = 1, Title = "Mouse Logitech G620", Price = 100.00M, isActive = true, CategoryId = 1 });
+                context.Products.Add(new Product { Id = 2, Title = "Notebook Gamer Acer FF517", Price = 4989.90M, isActive = true, CategoryId = 2 });
+                context.Products.Add(new Product { Id = 3, Title = "Placa de Vídeo GTX 1080TI", Price = 2450.60M, isActive = true, CategoryId = 2 });
+                context.Products.Add(new Product { Id = 4, Title = "Caneta Razer Kraken", Price = 50M, isActive = false, CategoryId = 3 });
+                context.Products.Add(new Product { Id = 5, Title = "Caderno Tilibra The PowerPuff Girls", Price = 16.40M, isActive = true, CategoryId = 3 });
+                context.SaveChanges();
+
+                base.Seed(context);
+            }
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //remove plural dos nomes das tabelas
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Configurations.Add(new ProductMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
